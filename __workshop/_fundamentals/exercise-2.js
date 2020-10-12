7a395bb7c951167b52bf55cc93d94b63fabf60fc
Athanasios Tommy Rigas <tommyrigas@Athanasioss-MacBook-Pro.local>
2020-10-12 19:01:49 -0400

    done

--- __workshop/_fundamentals/exercise-2.js
+++ __workshop/_fundamentals/exercise-2.js
@@ -54,6 +54,21 @@ const favoriteDessertsGroupB = {
 
 function sortByPopularity(obj) {
   // Write code
+  const values = Object.values(obj);
+  values.reverse();
+  let countDesserts = values.reduce((acc, dessert) => {
+    if (typeof acc[dessert] === "undefined") {
+      acc[dessert] = 1;
+    } else {
+      acc[dessert] += 1;
+    }
+    return acc;
+  }, {});
+
+  let finalAnswer = Object.keys(countDesserts).sort((keya, keyb) => {
+    return countDesserts[keyb] - countDesserts[keya];
+  });
+  return finalAnswer;
 }
 
 // Verification via console.log()
